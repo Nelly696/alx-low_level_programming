9071c2e9486c9c7e82eb282ae3d06d415d5646ae
Nelly696 <amarachinaomi819@gmail.com>
2023-09-21 19:21:43 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,38 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
- * print_number - prints an integer
- * @n:integer to be printed
- * Return:returns 0
+ * print_number - main function.
+ *
+ * @n: Function parameter
+ *
+ * Return: Always 0.
  */
 void print_number(int n)
+
 {
+
 	unsigned int n1;
+
 	n1 = n;
-	
+
 	if (n < 0)
+
 	{
+
 		_putchar('-');
+
 		n1 = -n;
+
 	}
 
-	if ((n1 / 10) > 0)
+	if (n1 / 10 != 0)
+
+	{
+
 		print_number(n1 / 10);
 
+	}
+
 	_putchar((n1 % 10) + '0');
+
 }
